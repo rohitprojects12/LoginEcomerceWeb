403ad85198d62c96597296beeb1ac8522210ada6
rohitprojects12 <147717777+rohitprojects12@users.noreply.github.com>
2023-10-18 19:14:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const signUpButton = document.getElementById("signUp");
+const signInButton = document.getElementById("signIn");
+const container = document.getElementById("container");
+
+signUpButton.addEventListener("click", () => {
+  // Redirect to signup website
+  window.location.href = "https://rohitprojects12.github.io/EcommerceWeb/";
+});
+
+signInButton.addEventListener("click", () => {
+  // Redirect to login website
+  window.location.href = "https://rohitprojects12.github.io/EcommerceWeb/";
+});
